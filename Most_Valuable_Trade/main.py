05e2c729ed6f315fbfcb52e63d747dd18c571fd4
OkkioXavier <dominicfarrington@gmail.com>
2021-07-31 00:49:43 +1000

    refactor: improve implementation of test files

--- Most_Valuable_Trade/main.py
+++ Most_Valuable_Trade/main.py
@@ -1,14 +1,3 @@
-lows = [1.0, 2.0, 5.0, 10.0, 11.0, 4.5, 4.3]
-highs = [10, 4.5, 6.1, 12, 100, 88, 5]
-starts = [2, 3, 5.5, 11, 11, 5, 4.8]
-
-print(f'Lows - {len(lows)}')
-print(lows)
-print(f'Highs - {len(highs)}')
-print(highs)
-print(f'Starts - {len(starts)}')
-print(starts)
-
 # Assertions
 # A low must always be less than a high for a given day
 # A start must always be greater than or equal to a low
@@ -16,19 +5,21 @@ print(starts)
 # Buys and sells must take place at the start of the day
 # A sell must take place after a buy
 
+# Question
+# Which is more accurate?
+# Returning the lowest loss or zero if no profit can be made?
+
 # Naive solution
 # Scan the array for the biggest in order rise between start and high
 
 
-naive_max = 0
-
-for index, start in enumerate(starts):
-    print(index)
-    for futureStart in starts[index + 1:]:
-        diff = futureStart - start
-        naive_max = diff if diff > naive_max else naive_max
-
-print(naive_max)
+def naive_search(array: []):
+    naive_max = 0
+    for index, start in enumerate(array):
+        print(index)
+        for futureStart in array[index + 1:]:
+            diff = futureStart - start
+            naive_max = diff if diff > naive_max else naive_max
 
 
 # Divide and conquer solution
@@ -63,20 +54,12 @@ def search_for_best_sale_price(array: []) -> Result:
     return Result(local_maximum, local_minimum, local_best)
 
 
-print('Divide and conquer')
+def linear_search(array: []):
+    minimum = float("inf")
+    max_profit = float("-inf")
+    for x in array:
+        minimum = x if x < minimum else minimum
+        max_profit = x - minimum if x - minimum > max_profit else max_profit
 
-result = search_for_best_sale_price(starts)
-
-print(result.best_sale_price)
-
-print('O(n) solution')
-
-minimum = starts[0]
-max_profit = 0
-
-for x in starts:
-    minimum = x if x < minimum else minimum
-    max_profit = x - minimum if x - minimum > max_profit else max_profit
-
-print(max_profit)
+    return max_profit
 
